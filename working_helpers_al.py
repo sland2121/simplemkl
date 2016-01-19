9b1a3f8469afcbb09670a4630a31228d0e085de1
Sayeri Lala <ls2121@mit.edu>
2016-01-19 15:24:39 -0800

    main fix: line search; otherwise minor changes

--- working_helpers_al.py
+++ working_helpers_al.py
@@ -17,10 +17,10 @@ from sklearn import svm
 
 weight_threshold=1e-08
 
-def fix_weight_precision(d):
+def fix_weight_precision(d,weight_precision):
     new_d=d.copy()
     # zero out weights below threshold
-    new_d[np.where(d<weight_threshold)[0]]=0
+    new_d[np.where(d<weight_precision)[0]]=0
     # normalize
     new_d=new_d/np.sum(new_d)
     return new_d
@@ -28,13 +28,18 @@ def fix_weight_precision(d):
 def compute_J_SVM(K,y_mat,C):
     clf=svm.SVC(C=C,kernel='precomputed')    
     clf.fit(K,y_mat[:,0])
+    #signed_alpha=clf.dual_coef_[0]*-1
     alpha=clf.dual_coef_[0]
     # dual coefficients should be nonnegative
     for i in np.where(alpha<0)[0]:
         alpha[i]*=-1
     alpha_indices=clf.support_
-
+    #return alpha_indices,alpha
+##    print "# svs",alpha_indices.shape[0]
+    #print "sv indices: ",np.sort(alpha_indices)
+    
     complete_alpha=np.zeros((y_mat[:,0].shape[0]))
+    
     #possible support vector indices
     #create ordered list of support vector coefficients
     for i in range(complete_alpha.shape[0]):
@@ -46,9 +51,11 @@ def compute_J_SVM(K,y_mat,C):
             assert len(tmp_index)==1
             tmp_index=tmp_index[0][0]
             complete_alpha[i]=alpha[tmp_index]
-   
-    J= -0.5*(complete_alpha.dot(np.multiply(K,y_mat)).dot(complete_alpha.T))+np.sum(complete_alpha)   
+##    print "not svs",np.setdiff1d()
+    J= -0.5*(np.absolute(complete_alpha).dot(np.multiply(K,y_mat)).dot(np.absolute(complete_alpha.T)))\
+       +np.sum(np.absolute(complete_alpha))   
     return complete_alpha,J
+    
 
 
 def compute_descent_direction(d, dJ,mu):
@@ -65,8 +72,8 @@ def compute_descent_direction(d, dJ,mu):
 
     return D
 
-def update_descent_direction(d,D,mu):
-    tmp_ind=np.intersect1d(np.where(D<=0)[0],np.where(d<=weight_threshold)[0])
+def update_descent_direction(d,D,mu,weight_precision):
+    tmp_ind=np.intersect1d(np.where(D<=0)[0],np.where(d<=weight_precision)[0])
     D[tmp_ind]=0
     
     if mu == 0:
@@ -86,27 +93,36 @@ def compute_max_admissible_gamma(d,D):
         
     return gamma_max
 
-def compute_gamma_linesearch(gamma_min,gamma_max,cost_min,cost_max,d,D,kernel_matrices,J_prev,y_mat,alpha):
+def compute_gamma_linesearch(gamma_min,gamma_max,delta_max,cost_min,cost_max,d,D,kernel_matrices,J_prev,y_mat,
+                             alpha,C,goldensearch_precision_factor):
     gold_ratio=(5**0.5+1)/2
-    
-    delta_max=gamma_max
+
+##    print "stepmin",gamma_min
+##    print "stepmax",gamma_max
+##    print "deltamax",delta_max
     gamma_arr=np.array([gamma_min,gamma_max])
     cost_arr=np.array([cost_min,cost_max])
 
     coord=np.argmin(cost_arr)
+##    print 'linesearch conditions'
+##    print 'gamma_min',gamma_min
+##    print 'gamma_max',gamma_max
+##    print 'delta_max',delta_max
+##    print 'golden search precision factor', goldensearch_precision_factor
     
-    while ((gamma_max-gamma_min) > 0.1*(abs(delta_max))):
+    while ((gamma_max-gamma_min) > goldensearch_precision_factor*(abs(delta_max)) and gamma_max>np.finfo(float).eps):
+        # print 'in line search loop'
         gamma_medr=gamma_min+(gamma_max-gamma_min)/gold_ratio;
         gamma_medl=gamma_min+(gamma_medr-gamma_min)/gold_ratio;
 
         tmp_d = d + gamma_medr*D
-        alpha_r,cost_medr=compute_J_SVM(k_helpers.get_combined_kernel(kernel_matrices,tmp_d),y_mat,10)
+        alpha_r,cost_medr=compute_J_SVM(k_helpers.get_combined_kernel(kernel_matrices,tmp_d),y_mat,C)
         tmp_d=d+gamma_medl*D
-        alpha_l,cost_medl=compute_J_SVM(k_helpers.get_combined_kernel(kernel_matrices,tmp_d),y_mat,10)
+        alpha_l,cost_medl=compute_J_SVM(k_helpers.get_combined_kernel(kernel_matrices,tmp_d),y_mat,C)
     
         cost_arr=np.array([cost_min, cost_medl, cost_medr, cost_max])
         gamma_arr=np.array([gamma_min, gamma_medl, gamma_medr, gamma_max])
-       
+
         coord=np.argmin(cost_arr)
 
         if coord==0:
@@ -138,15 +154,8 @@ def compute_dJ(kernel_matrices, y_mat, alpha,debug=False):
 
     for m in range(M):
         kernel_matrix = kernel_matrices[m]
-        if debug and m==1:
-            print 'dJ for 2nd kernel computation'
-#            print kernel_matrix[4,0:4]
-            print alpha.dot(np.multiply(kernel_matrix,y_mat)).dot(alpha.T)
-#        print 'kernel matrix: ',m
-#        print kernel_matrix[0:2,0:9]
-        dJ[m] = -0.5 * alpha.dot(np.multiply(kernel_matrix,y_mat)).dot(alpha.T) #based on matlab implementation
-        #dJ[m] = -0.5 * alpha.dot(np.multiply(kernel_matrix, y_mat)).dot(alpha.T)        
-        # assert dJ[m].shape[0] == 1
+        dJ[m] = -0.5 * alpha.dot(np.multiply(kernel_matrix,y_mat)).dot(alpha.T)
+        #dJ[m] = -0.5 * alpha.dot(kernel_matrix).dot(alpha.T)
 
     return dJ
 
