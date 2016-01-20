a4289723ee50200f08ef2c353ebf7e06a7d2bf17
Sayeri Lala <ls2121@mit.edu>
2016-01-19 18:20:02 -0800

    added tests checking behavior of interaction of k_init,C in learning

--- testing.py
+++ testing.py
@@ -27,7 +27,10 @@ test cases:
 		-C: 1,10,100,1000
 	-4 different kernels (learning occurs)
 		-kinit: uniform, non-uniform:[1 0 0], [0 1 0], [0 0 1]
-		-C: 1, 10, 100, 1000
+		-C:
+                    1,1000: kinit matters
+                    10,100: kinit does not matter
+                    
 		
 	
 """
@@ -86,23 +89,34 @@ np.testing.assert_array_almost_equal(weights,np.array([1.0]))
 #assert np.array_equal(weights,np.array([1.0]))
 
 # test case 2: equivalent kernels (no learning)
+# across different C
 
 #case a: uniform weights
-print 'case 2a'
+
 k_init=np.array([0.25,0.25,0.25,0.25])
 kernel_functions=[k_helpers.create_rbf_kernel(5.0), k_helpers.create_rbf_kernel(5.0),
                   k_helpers.create_rbf_kernel(5.0),k_helpers.create_rbf_kernel(5.0)]
 kernel_matrices=construct_gram_cube(xtrain,kernel_functions)
 C=10
+print 'case 2a'
 weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
 #assert np.array_equal(weights,np.array([0.25,0.25,0.25,0.25]))
 np.testing.assert_array_almost_equal(weights,np.array([0.25,0.25,0.25,0.25]))
-#case b: non-uniform weights
+
 print 'case 2b'
 k_init=np.array([0.4,0.3,0.2,0.1])
 weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
 np.testing.assert_array_almost_equal(weights,np.array([0.4,0.3,0.2,0.1]))
-#assert np.array_equal(weights,np.array([0.4,0.3,0.2,0.1]))
+
+C=1
+print 'case 2c'
+k_init=np.array([0.25,0.25,0.25,0.25])
+weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
+np.testing.assert_array_almost_equal(weights,np.array([0.25,0.25,0.25,0.25]))
+print 'case 2d'
+k_init=np.array([0.4,0.3,0.2,0.1])
+weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
+np.testing.assert_array_almost_equal(weights,np.array([0.4,0.3,0.2,0.1]))
 
 # test case 3: different kernels (learning)
 # 3 kernels
@@ -119,14 +133,15 @@ k_init=np.array([0,1,0])
 weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
 np.testing.assert_array_almost_equal(weights,np.array([0,1,0]))
 
-
-
-# test case 4: different kernels (learning)
+# NOTE: below test cases were manually checked against weights
+# due to precision issues
+# test case 4: different kernels (learning) across different C
 
 kernel_functions=[k_helpers.create_rbf_kernel(1.0),k_helpers.create_rbf_kernel(3.0),
                   k_helpers.create_rbf_kernel(5.0),k_helpers.create_rbf_kernel(10.0)]
 kernel_matrices=construct_gram_cube(xtrain,kernel_functions)
 
+# case 4c-d: k_init does not influence the weights
 C=10
 #case a: uniform weights
 print 'case 4a'
@@ -134,29 +149,60 @@ k_init=np.array([0.25,0.25,0.25,0.25])
 weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
 np.testing.assert_array_almost_equal(weights,np.array([0,1,0,0]))
 
-#case b: non-uniform weights
+#remaining cases: non-uniform weights
 print 'case 4b'
 k_init=np.array([0,0,0,1])
 weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
 np.testing.assert_array_almost_equal(weights,np.array([0,1,0,0]))
 
-# testing C values
+print 'case 4c'
 k_init=np.array([1,0,0,0])
+weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
+np.testing.assert_array_almost_equal(weights,np.array([0,1,0,0]))
+
+# testing interaction of k_init and C values
+
+# case 4c-1: k_init influences the weights in higher sigfigs
 print "C=1"
 C=1
+k_init=np.array([1,0,0,0])
 weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
+print weights
 #np.testing.assert_array_almost_equal(weights,np.array([0,0.5159,0.4247,0.0594]))
+k_init=np.array([0.25,0.25,0.25,0.25])
+weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
+print weights
+k_init=np.array([0,1,0,0])
+weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
+print weights
 
+
+# case 4c-2: k_init does not influence the weights
 print "C=100"
 C=100
+k_init=np.array([0,1,0,0])
 weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
+print weights
+k_init=np.array([1,0,0,0])
+weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
+print weights
+k_init=np.array([0.25,0.25,0.25,0.25])
+weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
+print weights
 #np.testing.assert_array_almost_equal(weights,np.array([0,1,0,0]))
 
-
+# case 4c-3: k_init influences weights in lower sigfigs
 print "C=1000"
 C=1000
-alpha=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
+k_init=np.array([0,1,0,0])
 weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
+print weights
+k_init=np.array([1,0,0,0])
+weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
+print weights
+k_init=np.array([0.25,0.25,0.25,0.25])
+weights,final_K,J,alpha,duality_gap=algo1.find_kernel_weights(k_init,kernel_matrices,C,ytrain)
+print weights
 #np.testing.assert_array_almost_equal(weights,np.array([0.62210,0.3779,0,0]))
 
 print 'All test cases passed.'
